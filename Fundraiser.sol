8e8c5ebdac444c56d7adcedecb1b03edcd5ab273
SanuSingh-git <sanu.singh.contact@gmail.com>
2026-05-25 13:48:17 +0530

    Update Fundraiser.sol

--- Fundraiser.sol
+++ Fundraiser.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.10;
 
-import "openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "./ReentrancyGuard.sol";
 
 contract Fundraiser is ReentrancyGuard{
     string public title;
